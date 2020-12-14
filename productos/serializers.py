39c56725fc8704abc79dad3683df7a3e0fc32190
Ancizar Rubio <ancizar.rubio@gmail.com>
2020-12-14 00:11:56 -0500

    travis

--- productos/serializers.py
+++ productos/serializers.py
@@ -1,10 +1,9 @@
 from rest_framework import serializers
 from productos.models import Producto
-from galleryProducts.serializers import ImagenesSeralizer
+from caractproducto.serializers import CaracteristicaSerializer
 
 
 class ProductoSerializer(serializers.ModelSerializer):
-    imagenes = ImagenesSeralizer(many=True)
     rama = serializers.SerializerMethodField()
     
     def get_rama(self, obj): 
