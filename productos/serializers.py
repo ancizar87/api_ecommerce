d37c3d9ff798baba8d4281b90d9ee19b0f70c47a
Ancizar Rubio <ancizar.rubio@gmail.com>
2020-12-13 23:35:48 -0500

    travis

--- productos/serializers.py
+++ productos/serializers.py
@@ -4,11 +4,7 @@ from productos.models import Producto
 
 
 class ProductoSerializer(serializers.ModelSerializer):
-    rama = serializers.SerializerMethodField()
-    
-    def get_rama(self, obj): 
-         return obj.rama.nombre
-    
+        
     class Meta:
         model = Producto
         fields = '__all__'
