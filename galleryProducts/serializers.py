a2b846d85b3f0f35e2461aafdc1c153e1c75b957
Ancizar Rubio <ancizar.rubio@gmail.com>
2020-12-14 13:03:25 -0500

    update

--- galleryProducts/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Imagenes
-
-class ImagenesSeralizer(serializers.ModelSerializer):
-    class Meta:
-        model = Imagenes
-        fields = '__all__'
