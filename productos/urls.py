da507289890ab5e1d3bc551ff21b60be1819fa4f
Ancizar Rubio <ancizar.rubio@gmail.com>
2020-12-14 00:06:07 -0500

    travis

--- productos/urls.py
+++ productos/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from productos.views import *
+from productos.views import ProductoListView, PaginadorListView, produclastListView, ProductofilterListView, Productofilter, producpromocionlastListView
 
 urlpatterns = [
     path('',ProductoListView.as_view()),
