a52ef1de4dcd333d23d82739175684e4e1bfd292
Ancizar Rubio <ancizar.rubio@gmail.com>
2020-12-14 22:38:18 -0500

    update

--- ramas/tests.py
+++ ramas/tests.py
@@ -1,1 +1,11 @@
 from django.test import TestCase
+from ramas.models import Ramal
+
+# Create your tests here.
+
+class RamalTestCase(TestCase):
+    def setUp(self):
+        Ramal.objects.create(nombre="Seguridad")
+        Ramal.objects.create(nombre="Almacenamiento")
+        Ramal.objects.create(nombre="Iluminacion")
+        
