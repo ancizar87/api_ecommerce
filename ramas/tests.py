0c2ec126de61a063400298b979a35c2b438f575e
Ancizar Rubio <ancizar.rubio@gmail.com>
2020-12-15 01:03:29 -0500

    update

--- ramas/tests.py
+++ ramas/tests.py
@@ -1,12 +1,5 @@
-from unittest.case import expectedFailure
+
 from django.test import SimpleTestCase
-from .views import views
+
 
 # Create your tests here.
-
-class TestViews(SimpleTestCase):
-    def setUp(self):
-        
-        def test_get_ok(self):
-            response = self.cobjects.get(nombre='nombre')
-            self.assertEqual(response.status_code, 200)
